c2d989acd0558c103d71195a965189336a0fc61a
AlekhyaPoosapati <69724903+AlekhyaPoosapati@users.noreply.github.com>
2021-04-17 15:44:29 +0530

    Create Secondpage.svelte

---
+++ src/Secondpage.svelte
@@ -0,0 +1,37 @@
+<script>
+    
+     export let name = " ";
+</script>
+
+<main>
+     <div class = "maindiv">
+          <div class = "Navbar">
+               
+
+          </div>
+          <div class = "greetings">
+               <h1>Hello {name}</h1>
+     
+          </div>
+          <div class = "options">
+     
+          </div>
+          <div class = "create">
+     
+          </div>
+     </div>
+     
+     
+</main>
+
+<style>
+     main
+	{
+		background-color: #1D1F25;
+		width: 100%;
+		height: 100vh;
+		margin-top: -31px;
+		margin-left: -7px;
+	}
+
+</style>
